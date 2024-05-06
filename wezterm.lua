e40562b62613b894107baac5d59f01ad41add041
alfayezDev <mohammed@alfayez.dev>
2024-05-06 13:40:07 +0300

    done

--- wezterm.lua
+++ wezterm.lua
@@ -8,15 +8,14 @@ if wezterm.config_builder then
 end
 
 -- Settings
-config.color_scheme = "Tokyo Night"
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font_with_fallback({
 	{ family = "Monolisa", scale = 1 },
 })
-config.window_background_opacity = 0.9
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 3000
-config.default_workspace = "home"
+config.default_workspace = "~/Documents/git"
 config.bidi_enabled = true
 -- Dim inactive panes
 config.inactive_pane_hsb = {
